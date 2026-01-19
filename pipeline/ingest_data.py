463b123303241cd537491ff1d23b5e39e2d3ec3f
jack-capodice <jack.capodice3@gmail.com>
2026-01-19 20:12:19 +0000

    pipeline code working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -23,27 +24,28 @@ dtype = {
     "total_amount": "float64",
     "congestion_surcharge": "float64"
 }
+
 parse_dates = [
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime"
 ]
 
-def run():   
-    pg_user='root'
-    pg_pass='root'
-    pg_host='localhost'
-    pg_port=5432
-    pg_db='ny_taxi'
 
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
 
-    year = 2021
-    month = 1
-
-    chunk_size = 100_000
-
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_{year}-{month}.csv.gz'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
@@ -52,30 +54,25 @@ def run():
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunk_size
+        chunksize=chunksize,
     )
 
     first = True
+
     for df_chunk in tqdm(df_iter):
         if first:
-            # Create table schema (no data)
             df_chunk.head(0).to_sql(
                 name=target_table,
                 con=engine,
-                if_exists="replace"
+                if_exists='replace'
             )
             first = False
-            print("Table created")
 
-        # Insert chunk
         df_chunk.to_sql(
             name=target_table,
             con=engine,
-            if_exists="append"
+            if_exists='append'
         )
-
-        print("Inserted:", len(df_chunk))
-
 
 if __name__ == '__main__':
     run()
